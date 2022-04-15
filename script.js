082eb00a445548284ff6ae1a1acc0a44c76e326c
Elayt <elaytpatters@gmail.com>
2022-04-15 22:09:25 +0500

    .

---
+++ script.js
@@ -0,0 +1,7 @@
+let jobInput = document.getElementById("job");
+let salaryInput = document.getElementById("salary");
+let teamInput = document.getElementById("team");
+let holidayInput = document.getElementById("holiday");
+let anwerBtn = document.getElementById("answer-btn");
+let okOut = document.getElementsById("ok-out");
+let failOut = document.getElementsById("fail-out");
