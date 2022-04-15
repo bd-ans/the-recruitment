0429a3b4154898749fe47f19479b51c13095a4a0
Elayt <elaytpatters@gmail.com>
2022-04-15 23:01:21 +0500

    .

--- script.js
+++ script.js
@@ -2,16 +2,16 @@ let jobInput = document.getElementById("job");
 let salaryInput = document.getElementById("salary");
 let teamInput = document.getElementById("team");
 let holidayInput = document.getElementById("holiday");
-let anwerBtn = document.getElementById("answer-btn");
+let answerBtn = document.getElementById("answer-btn");
 let okOut = document.querySelector(".ok-out");
-let FailOut = document.querySelector(".fail-out");
+let failOut = document.querySelector(".fail-out");
 
 okOut.classList.add("d-none");
-FailOut.classList.add("d-none");
+failOut.classList.add("d-none");
 okOut.classList.remove("d-block");
-FailOut.classList.remove("d-block");
+failOut.classList.remove("d-block");
 
-anwerBtn.addEventListener("click", function (e) {
+answerBtn.addEventListener("click", function (e) {
   e.preventDefault();
   let job = jobInput.checked;
   let salary = salaryInput.checked;
@@ -21,19 +21,19 @@ anwerBtn.addEventListener("click", function (e) {
   if (!job && !salary && !team && !holiday) {
     okOut.classList.remove("d-block");
     okOut.classList.add("d-none");
-    FailOut.classList.remove("d-block");
-    FailOut.classList.add("d-none");
+    failOut.classList.remove("d-block");
+    failOut.classList.add("d-none");
     return
   }
 
   if ((job && team && salary && !holiday) || (job && salary && team &&!holiday)) {
     okOut.classList.remove("d-none");
     okOut.classList.add("d-block");
-    FailOut.classList.remove("d-block");
-    FailOut.classList.add("d-none");
+    failOut.classList.remove("d-block");
+    failOut.classList.add("d-none");
   } else {
-    FailOut.classList.remove("d-none");
-    FailOut.classList.add("d-block");
+    failOut.classList.remove("d-none");
+    failOut.classList.add("d-block");
     okOut.classList.remove("d-block");
     okOut.classList.add("d-none");
   }
