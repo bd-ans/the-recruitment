c340ef9a53a16c3e7ac64c12358129b51f47f140
Elayt <elaytpatters@gmail.com>
2022-07-19 05:00:02 +0500

    GO

--- script.js
+++ script.js
@@ -10,14 +10,14 @@ okOut.classList.add("d-none");
 failOut.classList.add("d-none");
 okOut.classList.remove("d-block");
 failOut.classList.remove("d-block");
-
+ 
 answerBtn.addEventListener("click", function (e) {
   e.preventDefault();
   let job = jobInput.checked;
   let salary = salaryInput.checked;
   let team = teamInput.checked;
   let holiday = holidayInput.checked;
- 
+
   if (!job && !salary && !team && !holiday) {
     okOut.classList.remove("d-block");
     okOut.classList.add("d-none");
